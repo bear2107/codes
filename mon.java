5ba48d992079f3b8959d486754960cc1c7b27afa
bear2107 <Piyush.joshi@iiitb.org>
2016-09-19 05:30:37 -0700

    Update mon.java

--- mon.java
+++ mon.java
@@ -1,7 +1,17 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+/*
+Big Chandan is a dire lover of Biryani, especially Old Monk's Biryani. Today, he went over to have some of it. To his surprise, the waiter turns out be to be a coding geek and refuses to serves him unless Chandu solves his two- arrays problem, stated as:
 
+Given two non-increasing array of integers A,B i.e A[i] >= A[i+1] and B[i] >= B[i+1] and for all i, 0 ≤ i < n-1.
+
+The monkiness of two numbers is given by: M (A[i],B[j]) = j - i , if j >=i and B[j] >= A[i], or 0 otherwise. 
+
+
+Find the monkiness of the two arrays, that is given by: M (A,B)= max (M(A[i],B[j])) for 0≤ i, j< n-1.
+
+*/
 
 class TestClass {
     public static void main(String args[] ) throws Exception {
